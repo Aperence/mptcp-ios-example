7f89f1d10118e02f26517b56f2f6903b54c3da46
Aperence <anthony.doeraene@hotmail.com>
2024-08-01 15:19:05 +0200

    Initial Commit

---
+++ mptcp-example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mptcp-example
+//
+//  Created by Anthony Doeraene on 01/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mptcp-example/mptcp_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  mptcp_exampleApp.swift
+//  mptcp-example
+//
+//  Created by Anthony Doeraene on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct mptcp_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
